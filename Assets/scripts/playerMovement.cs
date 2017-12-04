fc54bbfcd6cc4ebb32e86d7f27ea0518c48d9480
Jake <jakelturner@gmail.com>
2017-12-04 19:10:17 +1100

    Initial commit

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public GameObject part;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("asd");
+
+        if (other.gameObject.tag == "enemy")
+        { 
+        GameObject tempObj = Instantiate(part, other.contacts[0].point, transform.rotation);
+        Destroy(tempObj, 3);
+     }
+    }
+}
