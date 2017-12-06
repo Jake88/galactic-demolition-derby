e99cd4eedb2c8ac2424b407a4bcdf9f94117e6ed
Jake <jakelturner@gmail.com>
2017-12-06 21:47:41 +1100

    Add gravity and WIP camera controller

--- Assets/scripts/cameraController.cs
+++ Assets/scripts/cameraController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
 	void Update () {
+        // TODO: Work in progress
+        // Pivot camera down, and make it sit higher as it zooms out (giving the player a look 'over' their planet)
+        // Set a max distance and min distance for zoom.
+
+        // Possibley decide do this by finding a good min position and good max position, store them as private positions (rotation + position)
+        // then do something like lerp between those points based on scroll wheel
+
         Vector3 direction = transform.position - transform.parent.position;
         direction.Normalize();
 
